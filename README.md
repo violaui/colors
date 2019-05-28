697ec7b772f636d2402b3854b2a64c2f1a6927f7
Amir Hossein Abdolkhalegh <ah.abdolkhalegh@gmail.com>
2019-05-28 10:38:24 +0430

    add colors functions and config the package

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # colors
 
+<a href="https://travis-ci.org/violacss/colors"><img src="https://img.shields.io/travis/violacss/colors.svg?style=flat-square" alt="Travis"></a>
+<a href="https://www.npmjs.com/package/@violacss/colors"><img src="https://img.shields.io/npm/v/@violacss/colors.svg?style=flat-square" alt="npm"></a>
+<a href="https://www.npmjs.com/package/@violacss/colors"><img src="https://img.shields.io/bundlephobia/minzip/@violacss/colors.svg?style=flat-square" alt="npm bundle size"></a>
+<a href="https://www.npmjs.com/package/@violacss/colors"><img src="https://img.shields.io/npm/dt/@violacss/colors.svg?color=%235e7de4&style=flat-square" alt="npm"></a>
+<a href="https://github.com/violacss/colors/blob/master/LICENSE"><img src="https://img.shields.io/github/license/violacss/colors.svg?color=%23aa55aa&style=flat-square" alt="License"></a>
+
 Every color-related stuff of the __Viola framework__ placed here.
+
+### How to install:
+
+```bash
+$ npm i --save-dev @violacss/colors
+```
+
+__or__
+
+```bash
+$ yarn add @violacss/colors --dev
+```
+
+__and the CDN link is:__
+
+```html
+<link href="https://unpkg.com/@violacss/colors@0.2.0/css/colors.min.css" rel="stylesheet">
+```
+
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const pkg = require("./package.json");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require("webpack");
+
+
+module.exports = {
+  mode: "production",
+  devtool: "source-map",
+  entry: "./colors.sass",
+  output: {
+    path: path.resolve(__dirname, "css"),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.BannerPlugin({
+      banner: `${pkg.name} v${pkg.version} | ${pkg.repository} | ${pkg.license} License`
+    }),
+    new MiniCssExtractPlugin({filename: "colors.min.css"}),
+    new OptimizeCssAssetsPlugin({
+      cssProcessorOptions: {map: true},
+      cssProcessorPluginOptions: {
+        preset: ["advanced", {
+          discardComments: {removeAll: false},
+          autoprefixer: {add: true}
+        }]
+      },
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.sass$/,
+        exclude: [/node_modules/],
+        use: [
+          {loader: MiniCssExtractPlugin.loader, options: {sourceMap: true}},
+          {loader: "css-loader", options: {sourceMap: true}},
+          {loader: "resolve-url-loader", options: {sourceMap: true}},
+          {
+            loader: "sass-loader",
+            options: {sourceMap: true, sourceMapContents: false, outputStyle: "compact"}
+          }
+        ]
+      }
+    ]
+  }
+}
